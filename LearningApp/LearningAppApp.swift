2fb67cf2a1d68924f70c8686fac0646457b23337
wynnbailey <wynn.bailey@gmail.com>
2021-12-30 12:41:25 -0800

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Wynn Bailey on 12/30/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
