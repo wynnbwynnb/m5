b12b8e87c73fccc6d5d5428c6df1186d8e9db00b
wynnbailey <wynn.bailey@gmail.com>
2021-12-31 16:41:17 -0800

    -work on content view with 1st card

--- LearningApp/Views/HomeView.swift
+++ LearningApp/Views/HomeView.swift
@@ -12,13 +12,55 @@ struct HomeView: View {
     @EnvironmentObject var model: ContentModel
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+        ScrollView {
+            LazyVStack {
+                ForEach(model.modules) { module in
+                    //
+                    // Learning Card
+                    //
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
+                            //
+                            //  This will scale the rectangle into the view
+                            //
+                            .aspectRatio(CGSize(width: 335, height: 175),contentMode: .fit)
+                     
+                        HStack {
+                            // Image goes here
+                            Image("swift")
+                                .resizable()
+                                .frame(width: 116, height: 116)
+                                .clipShape(Circle())
+                            Spacer()
+                            VStack (alignment: .leading) {
+                                Text("Learn Swift").bold()
+                                Text("in 22 hours!")
+                                    .padding(.bottom, 20)
+                                
+                                HStack{
+                                    Image(systemName: "text.book.closed")
+                                        .resizable()
+                                        .frame(width: 15, height: 15)
+                                    Text("20 Lessons").font(.caption)
+                                    Image(systemName: "clock")
+                                        .resizable()
+                                        .frame(width: 15, height: 15)
+                                    Text("2 hours").font(.caption)
+                                }// hstack
+                            }// vstack
+                        }.padding(.horizontal, 20)// hstack
+                    } // zstack
+                } // for each
+            } // lazy vsstack
+        } // scroll view
+    } // view
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        HomeView().environmentObject(ContentModel())
     }
 }
