952fd765c65136d92e6a8baa16450b9984c067ab
wynnbailey <wynn.bailey@gmail.com>
2022-01-01 12:54:37 -0800

    -break into HomeRowView -create two cards in HomeView -make it size correctly

--- LearningApp/Views/HomeView.swift
+++ LearningApp/Views/HomeView.swift
@@ -12,51 +12,32 @@ struct HomeView: View {
     @EnvironmentObject var model: ContentModel
     
     var body: some View {
-        ScrollView {
-            LazyVStack {
-                ForEach(model.modules) { module in
-                    //
-                    // Learning Card
-                    //
-                    ZStack {
-                        Rectangle()
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
-                            .shadow(radius: 5)
+        NavigationView{
+            VStack (alignment:.leading) {
+                Text("What do you want to do today?")
+                    .padding(.leading, 20)
+                ScrollView {
+                    LazyVStack {
+                        ForEach(model.modules) { module in
                             //
-                            //  This will scale the rectangle into the view
+                            // Learning Card
                             //
-                            .aspectRatio(CGSize(width: 335, height: 175),contentMode: .fit)
-                     
-                        HStack {
-                            // Image goes here
-                            Image("swift")
-                                .resizable()
-                                .frame(width: 116, height: 116)
-                                .clipShape(Circle())
                             Spacer()
-                            VStack (alignment: .leading) {
-                                Text("Learn Swift").bold()
-                                Text("in 22 hours!")
-                                    .padding(.bottom, 20)
-                                
-                                HStack{
-                                    Image(systemName: "text.book.closed")
-                                        .resizable()
-                                        .frame(width: 15, height: 15)
-                                    Text("20 Lessons").font(.caption)
-                                    Image(systemName: "clock")
-                                        .resizable()
-                                        .frame(width: 15, height: 15)
-                                    Text("2 hours").font(.caption)
-                                }// hstack
-                            }// vstack
-                        }.padding(.horizontal, 20)// hstack
-                    } // zstack
-                } // for each
-            } // lazy vsstack
-        } // scroll view
-    } // view
+                            VStack (spacing: 20) {
+                                HomeViewRow(image: module.content.image, title: "Learn \(module.category)", description: module.content.description, count: "\(module.content.lessons.count) Lessons", time: module.content.time)
+                          
+                            //
+                            // Learning Card
+                            //
+                                HomeViewRow(image: module.test.image, title: "\(module.category) Test", description: module.test.description, count: "\(module.test.questions.count) Questions", time: module.test.time)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Get Started")
+        }
+    }
 }
 
 struct HomeView_Previews: PreviewProvider {
